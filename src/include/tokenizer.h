344d1e261b15ad4837493bd15a37cc2c6f40fbc9
gurbur <gurbur331@ajou.ac.kr>
2025-06-30 17:57:52 +0900

    Feat: types for tokenizer

---
+++ src/include/tokenizer.h
@@ -0,0 +1,28 @@
+#include "list_head.h"
+
+typedef enum {
+	TOKEN_HASH,        // #
+	TOKEN_ASTERISK,    // *
+	TOKEN_TEXT,        // normal text
+	TOKEN_NUMBER,      // number (used for number list)
+	TOKEN_DOT,         // . (used for number list)
+	TOKEN_DASH,        // - (used for unnumbered list)
+	TOKEN_NEWLINE,     // \n
+	TOKEN_TAB,         // \t
+	TOKEN_LBRACKET,    // [
+	TOKEN_RBRACKET,    // ]
+	TOKEN_LPAREN,      // (
+	TOKEN_RPAREN,      // )
+	TOKEN_BACKTICK,    // `
+	TOKEN_EXCLAMATION, // !
+	TOKEN_GREATER_THAN,// >
+	TOKEN_BACKSLASH,   // \\
+	TOKEN_EOF,         // End of File
+} TokenType;
+
+typedef struct {
+	TokenType type;
+	char* value;
+	struct list_head list;
+} Token;
+
